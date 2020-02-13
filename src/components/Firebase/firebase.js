062f7d4119efa5016620b47d5472b36e42341620
Bianca Gotaski <bgotaski@biancas-air.br.ibm.com>
2020-02-13 15:58:00 -0300

    trying to do the build/run the project locally by firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -11,7 +11,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
+const prodConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -22,14 +22,22 @@ const config = {
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// This example below is when you have 2 environments (prod and dev)
-// In that case you should have 2 firebase configs (which you should setup on firebase as well...)
-// const configEnv = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+const devConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 export default class Firebase{
     constructor(){
         app.initializeApp(config);
-        
         this.auth = app.auth();
     }
 
